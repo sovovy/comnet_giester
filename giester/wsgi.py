03854d1b5082f8dc87e859c41dc7cce2ebc06359
sovovy <sovovy@gmail.com>
2016-05-21 22:17:52 +0900

    front

---
+++ giester/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for giester project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "giester.settings")
+
+application = get_wsgi_application()
