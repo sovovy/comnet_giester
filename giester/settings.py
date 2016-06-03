df5f357009bfda0b4056dd4ea90d066aea67eff1
sovovy <sovovy@gmail.com>
2016-06-03 21:23:42 +0900

    static 파일 사용 가능하게 변경
    원래 외부 url로 사진, css 등을 가져왔는데 이제 static 폴더에서 가져올 수 있습니다.

--- giester/settings.py
+++ giester/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_statics')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
