91052080e331df45c3dcac9ade55e4e77ea87a47
sovovy <sovovy@gmail.com>
2016-06-04 20:48:02 +0900

    board 반영이 안되서 다시

--- game/models.py
+++ game/models.py
@@ -4,4 +4,5 @@ class Player(models.Model):
 	po = models.CharField(max_length=10, default='')
 	pt = models.CharField(max_length=10, default='')
 	turn = models.CharField(max_length=3, default='00')
+	board = models.TextField(default='944448033330000000000000011110922228')
 	# 1-1P 파, 2-1P빨, 3-2P 파, 4-2P 빨
