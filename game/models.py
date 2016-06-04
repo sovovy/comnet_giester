4f87a23cc2531f2cecb5ad67deaaf8d51160f04d
sovovy <sovovy@gmail.com>
2016-06-04 20:34:30 +0900

    board default 변경
    8 - 오른쪽 출구, 9 - 왼쪽 출구 를 반영하였습니다.

--- game/models.py
+++ game/models.py
@@ -4,5 +4,4 @@ class Player(models.Model):
 	po = models.CharField(max_length=10, default='')
 	pt = models.CharField(max_length=10, default='')
 	turn = models.CharField(max_length=3, default='00')
-	board = models.TextField(default='044440033330000000000000011110022220')
 	# 1-1P 파, 2-1P빨, 3-2P 파, 4-2P 빨
