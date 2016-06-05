7478ac7d04b7adaf08080efac58e8abf1160ac09
sovovy <sovovy@gmail.com>
2016-06-06 00:18:30 +0900

    urls.py문제점 해결, css

--- game/urls.py
+++ game/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^init', views.init),
-    url(r'^rfsh', views.rfsh),
-    url(r'^chkDB', views.chkDB),
-    url(r'^set', views.set),
-    url(r'^setChk', views.setChk),
-    url(r'^game', views.game),
-    url(r'^wait', views.wait),
-    url(r'^deal', views.deal)
+    url(r'^init$', views.init),
+    url(r'^rfsh$', views.rfsh),
+    url(r'^chkDB$', views.chkDB),
+    url(r'^set$', views.set),
+    url(r'^setChk$', views.setChk),
+    url(r'^game$', views.game),
+    url(r'^wait$', views.wait),
+    url(r'^deal$', views.deal)
 ]
